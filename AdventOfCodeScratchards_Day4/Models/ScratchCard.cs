3fc8e1361039c49e823a1cdc15f53184f574ca64
RokasCvirka <rokas42@gmail.com>
2023-12-11 13:55:28 +0200

    fix repetitions count

--- AdventOfCodeScratchards_Day4/Models/ScratchCard.cs
+++ AdventOfCodeScratchards_Day4/Models/ScratchCard.cs
@@ -40,7 +40,16 @@ namespace AdventOfCodeScratchards_Day4.Models
         {
             if(repetitions == 0) return 0;
             if(repetitions == 1) return 1;
-            return Math.Pow(2, repetitions);
+            if(repetitions == 2) return 2;
+            if (repetitions == 3) return 4;
+            if (repetitions == 4) return 8;
+            if (repetitions == 5) return 16;
+            if (repetitions == 6) return 32;
+            if (repetitions == 7) return 64;
+            if (repetitions == 8) return 128;
+            if (repetitions == 9) return 256;
+            if (repetitions == 10) return 512;
+            return 0;   
         }
     }
 }
